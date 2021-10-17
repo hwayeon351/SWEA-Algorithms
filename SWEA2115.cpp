ecacae87560ad6c8a25bc673690423c1daccc7e5
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2021-10-17 15:37:20 +0900

    Update SWEA2115.cpp

--- SWEA2115.cpp
+++ SWEA2115.cpp
@@ -2,102 +2,75 @@
 //  main.cpp
 //  SWEA2115
 //
-//  Created by Hwayeon on 2021/01/13.
+//  Created by Hwayeon on 2021/10/17.
 //
+
 #include <iostream>
-#include <vector>
-#include <math.h>
 using namespace std;
- 
+
 int N, M, C;
-int map[10][10] = {0,};
-bool visit[10][10] = {false,};
-int worker_earn[2] = {0,};
-int max_earn = 0;
-bool wh[2][5] = {false,};
-vector<vector<pair<int, int> > > worker;
- 
-void choice_honey(int w, int cnt, int earn){
-    if(cnt == M){
-        int total = 0;
-        for(int i=0; i<M; i++){
-            if(wh[w][i] == true){
-                total += pow(map[worker[w][i].first][worker[w][i].second], 2);
-            }
-        }
-        if(total > worker_earn[w]) worker_earn[w] = total;
-        return;
+int honey[11][11] = {0, };
+int answer = 0;
+int w1_earn = 0;
+int w2_earn = 0;
+
+void w1_earn_money(int x, int sx, int sy, int h, int money){
+    if(x > sx+M) return;
+    if(h > C) return;
+    if(money > w1_earn) w1_earn = money;
+    for(int xx=x; xx<sx+M; xx++){
+        w1_earn_money(xx+1, sx, sy, h+honey[sy][xx], money + honey[sy][xx]*honey[sy][xx]);
     }
-    for(int i=0; i<M; i++){
-        if(wh[w][i] == true) continue;
-        if(map[worker[w][i].first][worker[w][i].second] + earn <= C){
-            wh[w][i] = true;
-            choice_honey(w, cnt+1, map[worker[w][i].first][worker[w][i].second] + earn);
-            wh[w][i] = false;
-        }
-        else choice_honey(w, cnt+1, earn);
+};
+
+void w2_earn_money(int x, int sx, int sy, int h, int money){
+    if(x > sx+M) return;
+    if(h > C) return;
+    if(money > w2_earn) w2_earn = money;
+    for(int xx=x; xx<sx+M; xx++){
+        w2_earn_money(xx+1, sx, sy, h+honey[sy][xx], money + honey[sy][xx]*honey[sy][xx]);
     }
-}
- 
-void get_max_earn(){
-    for(int i=0; i<2; i++){
-        worker_earn[i] = 0;
-        choice_honey(i, 0, 0);
+};
+
+void get_honey(int wx, int wy){
+    w1_earn = 0;
+    w1_earn_money(wx, wx, wy, 0, 0);
+    int ny = wy;
+    int nx = wx+M;
+    if(nx > N-M){
+        nx = 0;
+        ny ++;
     }
-    if(worker_earn[0]+worker_earn[1] > max_earn) max_earn = worker_earn[0]+worker_earn[1];
-}
- 
-void pick_honey(int cnt){
-    vector<pair<int, int> > v;
-    v.clear();
-    if(cnt==2) {
-        get_max_earn();
-        return;
-    }
-    for(int i=0; i<N; i++){
-        for(int j=0; j<=N-M; j++){
-            bool check = true;
-            for(int m=j; m<j+M; m++){
-                if(visit[i][m]==true){
-                    check = false;
-                    break;
-                }
-            }
-            if(check == false) continue;
-            else{
-                for(int m=j; m<j+M; m++){
-                    visit[i][m] = true;
-                    v.push_back(make_pair(i, m));
-                }
-                worker.push_back(v);
-                pick_honey(cnt+1);
-                for(int m=j; m<j+M; m++){
-                    visit[i][m] = false;
-                }
-                worker.pop_back();
-                v.clear();
-            }
+    for(int y=ny; y<N; y++){
+        if(y > ny) nx = 0;
+        for(int x=nx; x<=N-M; x++){
+            w2_earn = 0;
+            w2_earn_money(x, x, y, 0, 0);
+            int total = w1_earn+w2_earn;
+            if(answer < total) answer = total;
         }
     }
 }
- 
-int main(int argc, char** argv)
-{
+
+int main(int argc, const char * argv[]) {
     int test_case;
     int T;
-    cin>>T;
- 
-    for(test_case = 1; test_case <= T; ++test_case)
-    {
-        max_earn = 0;
+    cin >> T;
+    for(test_case=1; test_case<=T; ++test_case){
+        answer = 0;
         cin >> N >> M >> C;
-        for(int i=0; i<N; i++){
-            for(int j=0; j<N; j++){
-                cin >> map[i][j];
+        for(int y=0; y<N; y++){
+            for(int x=0; x<N; x++){
+                cin >> honey[y][x];
             }
         }
-        pick_honey(0);
-        cout << "#" << test_case << " " << max_earn << endl;
+        for(int y=0; y<N; y++){
+            for(int x=0; x<=N-M; x++){
+                if(y == N-1 && x > N-2*M) break;
+                get_honey(x, y);
+            }
+        }
+        cout << "#" << test_case << " " << answer << endl;
     }
     return 0;
 }
