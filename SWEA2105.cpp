1669c44f957c24567a24be68a3fb9d085cdd4c06
hwayeon351 <55572163+hwayeon351@users.noreply.github.com>
2022-04-30 17:54:23 +0900

    Update SWEA2105.cpp

--- SWEA2105.cpp
+++ SWEA2105.cpp
@@ -1,76 +1,103 @@
-//
-//  main.cpp
-//  SWEA2105_1
-//
-//  Created by Hwayeon on 2021/10/19.
-//
-
 #include <iostream>
-#include <string.h>
-#include <vector>
+#include <cstring>
 using namespace std;
 
 int N;
-int desert[21][21] = {0, };
-int visit[101] = {0, };
-int dx[4] = {-1, 1, 1, -1};
-int dy[4] = {1, 1, -1, -1};
+int desert[20][20] = { 0, };
+int visit[101] = { 0, };
+int dr[4] = { 1, 1, -1, -1 };
+int dc[4] = { -1, 1, 1, -1 };
 
-int max_deserts = -1;
+int answer = -1;
 
-void go_tour(){
-    int nx, ny, cnt;
-    bool check;
-    for(int d1=1; d1<=N-1; d1++){
-        for(int d2=1; d2<=N-1; d2++){
-            for(int y=0; y<N-1; y++){
-                for(int x=1; x<N-1; x++){
-                    memset(visit, 0, sizeof(visit));
-                    nx = x;
-                    ny = y;
-                    cnt = 1;
-                    check = true;
-                    visit[desert[y][x]] = 1;
-                    for(int i=0; i<4; i++){
-                        int d;
-                        if(i==0) d=d1;
-                        else if(i==1) d=d2;
-                        else if(i==2) d=d1;
-                        else d=d2-1;
-                        for(int j=0; j<d; j++){
-                            nx += dx[i];
-                            ny += dy[i];
-                            if(nx<0 || nx>=N || ny<0 || ny>=N || visit[desert[ny][nx]]) {
-                                check = false;
-                                break;
-                            }
-                            visit[desert[ny][nx]] = 1;
-                            cnt++;
-                        }
-                        if(!check) break;
-                    }
-                    if(!check) continue;
-                    if(max_deserts < cnt) max_deserts = cnt;
-                }
-            }
-        }
-    }
+void init() {
+	cin >> N;
+	for (int r = 0; r < N; r++) {
+		for (int c = 0; c < N; c++) {
+			cin >> desert[r][c];
+		}
+	}
+	answer = -1;
 }
 
-int main(int argc, const char * argv[]) {
-    int test_case;
-    int T;
-    cin >> T;
-    for(test_case=1; test_case<=T; ++test_case){
-        max_deserts = -1;
-        cin >> N;
-        for(int y=0; y<N; y++){
-            for(int x=0; x<N; x++){
-                cin >> desert[y][x];
-            }
-        }
-        go_tour();
-        cout << "#" << test_case << " " << max_deserts << endl;
-    }
-    return 0;
+void go_cafe() {
+	for (int r = 0; r < N; r++) {
+		for (int c = 0; c < N; c++) {
+			for (int d1 = 1; d1 <= N - 2; d1++) {
+				for (int d2 = 1; d2 <= N - 2; d2++) {
+					memset(visit, 0, sizeof(visit));
+					int nr = r;
+					int nc = c;
+					int cnt = 1;
+					visit[desert[nr][nc]] = 1;
+					bool check = true;
+
+					for (int d = 0; d < d1; d++) {
+						nr += dr[0];
+						nc += dc[0];
+						if (nr < 0 || nr >= N || nc < 0 || nc >= N || visit[desert[nr][nc]]) {
+							check = false;
+							break;
+						}
+						cnt++;
+						visit[desert[nr][nc]] = 1;
+					}
+					if (!check) continue;
+
+					for (int d = 0; d < d2; d++) {
+						nr += dr[1];
+						nc += dc[1];
+						if (nr < 0 || nr >= N || nc < 0 || nc >= N || visit[desert[nr][nc]]) {
+							check = false;
+							break;
+						}
+						cnt++;
+						visit[desert[nr][nc]] = 1;
+					}
+					if (!check) continue;
+
+					for (int d = 0; d < d1; d++) {
+						nr += dr[2];
+						nc += dc[2];
+						if (nr < 0 || nr >= N || nc < 0 || nc >= N || visit[desert[nr][nc]]) {
+							check = false;
+							break;
+						}
+						cnt++;
+						visit[desert[nr][nc]] = 1;
+					}
+					if (!check) continue;
+
+					for (int d = 0; d < d2-1; d++) {
+						nr += dr[3];
+						nc += dc[3];
+						if (nr < 0 || nr >= N || nc < 0 || nc >= N || visit[desert[nr][nc]]) {
+							check = false;
+							break;
+						}
+						cnt++;
+						visit[desert[nr][nc]] = 1;
+					}
+					if (!check) continue;
+
+					if (answer == -1 || answer < cnt) answer = cnt;
+				}
+			}
+		}
+	}
+}
+
+int main()
+{
+	int T;
+	cin >> T;
+
+	for (int test_case = 1; test_case <= T; test_case++) {
+		init();
+		go_cafe();
+
+		cout << "#" << test_case << " " << answer << "\n";
+	}
+
+	return 0;
 }
